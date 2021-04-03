965a0a6c2c7d7ce62b429d95044cc42bcddfcde5
ioqren <corriestoddard@gmail.com>
2021-04-02 22:36:26 -0700

    Add db migrations

--- db/migrate/20210403045657_create_users.rb
+++ db/migrate/20210403045657_create_users.rb
@@ -1,15 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.integer :id
-      t.string :username
-      t.string :email
-      t.string :password_digest
-      t.string :session_token
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
 
       t.timestamps
     end
+    add_index :users, :email, unique: true
   end
 end
--- db/migrate/20210403045811_create_patterns.rb
+++ db/migrate/20210403045811_create_patterns.rb
@@ -1,11 +1,11 @@
 class CreatePatterns < ActiveRecord::Migration[6.1]
   def change
     create_table :patterns do |t|
-      t.integer :id
-      t.string :title
-      t.integer :author_id
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.string :title, null:false
+      t.binary :img, null:false
+      t.integer :author_id, null:false
+      t.datetime :created_at, null:false
+      t.datetime :updated_at, null:false
 
       t.timestamps
     end
