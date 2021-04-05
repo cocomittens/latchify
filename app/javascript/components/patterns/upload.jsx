4380f50284143d179518a9c370abf7110cbd7664
ioqren <corriestoddard@gmail.com>
2021-04-05 02:20:51 -0700

    Implement image upload functionality

--- app/javascript/components/patterns/upload.jsx
+++ app/javascript/components/patterns/upload.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@material-ui/core";
+import ImageUploading from "react-images-uploading";
 
 export default () => {
+  const [images, setImages] = useState([]);
+  const maxNumber = 69;
+
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
   return (
     <div className="upload">
-      <Button>Upload</Button>
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          // write your building UI
+          <div className="upload__image-wrapper">
+            <Button
+              style={isDragging ? { color: "red" } : undefined}
+              onClick={onImageUpload}
+              {...dragProps}
+              variant="contained"
+            >
+              Click or Drop here
+            </Button>
+          </div>
+        )}
+      </ImageUploading>
     </div>
   );
 };
