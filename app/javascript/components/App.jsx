ad3165f104d8e180b32ad91f9a43a2d317e9f7bf
ioqren <corriestoddard@gmail.com>
2021-04-05 02:27:52 -0700

    Format results page

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,12 +4,14 @@ import { Route, Switch } from "react-router-dom";
 import SignupContainer from "./session/signup_container";
 import LoginContainer from "./session/login_container";
 import Upload from "./patterns/upload";
+import Results from "./patterns/results";
 
 export default () => (
   <Switch>
     <Route path="/signup" component={SignupContainer} />
     <Route path="/login" component={LoginContainer} />
     <Route path="/upload" component={Upload} />
+    <Route path="/results" component={Results} />
     <Route exact path="/" component={Home} />
   </Switch>
 );
